10db4be68cd0c6c8e86a152794f7e4ca97985e3a
Viviano Cantu <viviano.cantu@gmail.com>
2017-05-17 17:42:42 -0400

    complete backup script

--- backup.sh
+++ backup.sh
@@ -3,4 +3,19 @@
 echo "Backing up .bashrc..."
 cp ~/.bashrc .
 
-echo "Backung up .tmux.conf..."
+echo "Backing up .tmux.conf..."
+cp ~/.tmux.conf .
+
+echo "Backing up .xinitrc"
+cp ~/.xinitrc .
+
+echo "Backing up .config/i3/config"
+cp ~/.config/i3/config .
+
+
+
+echo "Saving to GitHub..."
+dt=$(date '+%dd/%mm/%Y %H:%M:%S');
+git add .
+git commit -m "autobackup $dt"
+git push origin master
