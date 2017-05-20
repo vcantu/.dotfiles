1829478ba0e3684ea84db6adc1ebc042d00c8c1f
Viviano Cantu <viviano.cantu@gmail.com>
2017-05-20 16:51:38 -0400

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 echo "Installing Packages...."
 sudo apt-get install vim software-properties-common python-software-properties libbsd-dev libfuse2 pkg-config -y
 sudo apt-get install build-essential libfuse-dev libsqlite3-dev -y
-sudo apt-get install python-pip ibssl-dev vim curl tmux -y
+sudo apt-get install python-pip vim curl tmux -y
 sudo apt-get install cpufrequtils iptables ssh ruby ruby-dev libpq-dev nodejs npm -y
 sudo apt-get install python3 python3-xdg -y
-sudo appt-get install openjdk-8-jdk -y
+sudo apt-get install openjdk-8-jdk -y
 
 echo "Installing Grive"
 sudo add-apt-repository ppa:nilarimogard/webupd8 -y
