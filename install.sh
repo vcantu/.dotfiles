7d9288b053e14229bc4e7b376a7fdee1d655d0bb
Viviano Cantu <vcantu@users.noreply.github.com>
2017-11-06 16:12:17 -0700

    remove python and pip installation

--- install.sh
+++ install.sh
@@ -2,21 +2,11 @@
 
 # Install packages
 echo "Installing Packages...."
-sudo apt-get install vim software-properties-common python-software-properties libbsd-dev libfuse2 pkg-config -y
+sudo apt-get install software-properties-common python-software-properties libbsd-dev libfuse2 pkg-config -y
 sudo apt-get install build-essential libfuse-dev libsqlite3-dev -y
-sudo apt-get install python-pip vim curl tmux -y
+sudo apt-get install vim curl tmux -y
 sudo apt-get install cpufrequtils iptables ssh ruby ruby-dev libpq-dev nodejs npm -y
-sudo apt-get install python3 python3-xdg rofi -y
 sudo apt-get install openjdk-8-jdk -y
-
-echo "Installing Grive"
-sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-sudo apt-get update -y
-sudo apt-get install grive -y
-
-echo "Installing Python Packages..."
-sudo pip install --upgrade pip
-sudo pip install bottle pyyaml -y
 
 echo "Installing Ruby..."
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
@@ -42,6 +32,7 @@ echo "Updating git credentials"
 git config --global user.email "viviano.cantu@gmail.com"
 git config --global user.name "Viviano Cantu"
 
+echo " ------- Install Complete ------"
 echo "Remember to uncomment en_US.UTF-8 UTF-8 in /etc/locale.gen"
 echo " then run sudo locale-gen"
 . ~/.bashrc
